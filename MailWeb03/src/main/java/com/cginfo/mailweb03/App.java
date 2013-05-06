0404524e50495cc872a1d815e89500fbed9aa25e
wantofly <conixfans@gmail.com>
2013-05-06 16:42:45 +0800

    MailWebTest

---
+++ MailWeb03/src/main/java/com/cginfo/mailweb03/App.java
@@ -0,0 +1,13 @@
+package com.cginfo.mailweb03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
